b62d7a9f210f13cf1b09da6161780eb2aaeaedfd
Constance Jiang <conzjiang@gmail.com>
2014-05-15 11:51:35 -0400

    create possible_spaces, valid_slide_spaces & refactor perform_slide in Piece class

--- piece.rb
+++ piece.rb
@@ -16,39 +16,65 @@ class Piece
     self.color == :black ? "⚫" : "⚪"
   end
   
-  def possible_spaces
+  def possible_spaces(n = 1)
     x, y = self.pos
     
-    move_diffs.map { |dir| [x + dir[0], y + dir[1]] }
+    # n = number of spaces away you want to evaluate
+    move_diffs.map { |dir| [x + dir[0] * n, y + dir[1] * n] }
   end
   
   def valid_slide_spaces
     possible_spaces.select { |space| self.board.empty?(space) }
   end
   
-  def valid_jump_spaces
-    possible_spaces.select do |space| 
+  def valid_jump_spaces(start_pos = self.pos)
+    # evaluates the spaces one space away where enemy resides
+    # returns Piece's new possible spaces after jumping over enemy
+    
+    enemy_spaces = possible_spaces.select do |space| 
       !self.board.empty?(space) && self.board.has_enemy?(space, self.color)
+    end
+    
+    jump_to_spaces = []
+    
+    enemy_spaces.each do |x, y|
+      dir_x, dir_y = x - start_pos[0], y - start_pos[1]
+      jump_to_space = [x + dir_x, y + dir_y]
+      
+      jump_to_spaces << jump_to_space if self.board.empty?(jump_to_space)
+    end
+    
+    jump_to_spaces
+  end
+  
+  def valid_mega_jump_spaces
+    one_space_aways = possible_spaces.select { |space| self.board.empty?(space) }
+    
+    one_space_aways.map do |space|
+      dir_x, dir_y = x - start_pos[0], y - start_pos[1]
+      enemy_space = [x + dir_x, y + dir_y]
+      
+      
+    end  
   end
   
   def perform_slide(start_pos, end_pos)
     x, y = start_pos
     
-    good_to_go = valid_spaces(start_pos).include?(end_pos)
-    
-    if good_to_go && self.board.empty?(end_pos)
+    if valid_slide_spaces.include?(end_pos)
       self.pos = end_pos
       self.board[end_pos] = self
       self.board[start_pos] = nil
+      return true
     end
     
-    good_to_go
+    false
   end
   
   def perform_jump(start_pos, end_pos)
     x, y = start_pos
     
-    good_to_go = valid_spaces(start_pos).
+    good_to_go = valid_spaces(start_pos)
 
     direction = move_diffs.find do |dir|
       space = [x + dir[0], y + dir[1]]
