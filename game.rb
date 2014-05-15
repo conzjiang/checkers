78511c0c63bc39d16ea4d18743ff0282fd95eb86
Constance Jiang <conzjiang@gmail.com>
2014-05-15 15:26:34 -0400

    create Game class, create parse and won? methods

--- game.rb
+++ game.rb
@@ -1,2 +1,39 @@
 # NOTES
 # raise error if start_pos is empty!!!
+#
+
+require './board'
+ 
+class Game
+  attr_reader :board
+  
+  def initialize(board = Board.new)
+    @board = board
+  end
+  
+  def play
+    player = :black
+    
+    until won?
+      prompt(player)
+      
+      turn = turn == :black ? :white : :black
+    end
+  end
+  
+  def prompt(color)
+    puts "What's the position of the piece you want to move?"
+    start_pos = parse(gets.chomp.strip)
+  end
+  
+  def parse(input)
+    input.split("-").map do |coord|
+      coord.strip.split(",").map { |x| x.strip.to_i }
+    end
+  end
+  
+  def won?
+    color_check = board.pieces[0].color
+    board.pieces.all? { |piece| piece.color == color_check }
+  end
+end
